fc348d1438dff9d58c93d82e5ec607c3d3a90f77
Sorin <sorinnechiforora@gmail.com>
2023-04-22 13:37:54 +0100

    Add tailwind.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1 className="font-lato">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
