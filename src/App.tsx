0f15e8d59df28f721a7a89a822d37c94d4bf3947
Sorin <sorinnechiforora@gmail.com>
2023-04-22 19:36:29 +0100

    Add form elements for search

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import SearchIcon from "./assets/search.svg";
+
 function App() {
   return (
-    <div className="w-screen h-screen flex flex-col gap-4">
+    <div className="w-screen h-screen flex flex-col gap-4" role="main">
       <div className="flex flex-col items-center pt-1">
         <h1 className="text-5xl font-bold">Marvel Comics Search Tool</h1>
         <p className="text-neutral-500">
@@ -9,27 +11,35 @@ function App() {
       </div>
 
       <div className="flex justify-center">
-        <form className="flex">
+        <form className="flex justify-center w-full">
           <select
-            className="appearance-none"
+            className="appearance-none flex-initial lg:w-40 lg:h-10 px-1 mr-1
+            border border-slate-200 rounded bg-white focus:outline-none
+            bg-[url('./assets/down-chevron.svg')] bg-right bg-origin-padding bg-no-repeat"
             defaultValue="Characters"
-            name="filter"
-            id="filter"
+            name="category"
+            id="category"
+            aria-label="Dropdown menu for selecting categories to search by"
           >
             <option value="characters">Characters</option>
             <option value="comics">Comics</option>
             <option value="events">Events</option>
             <option value="creators">Creators</option>
           </select>
-          <label htmlFor="search-input"></label>
           <input
-            className="appearance-none"
+            className="appearance-none px-1 focus:outline-none border border-slate-200 lg:h-10 lg:w-1/4"
             type="search"
             placeholder="Search"
             id="search-input"
+            aria-label="Search input"
           />
-          <button className="bg-sky-400" type="submit" id="submit-btn">
-            <img src="https://img.icons8.com/ios-glyphs/30/null/search--v1.png" />
+          <button
+            className="bg-sky-400 w-10 lg:h-10 flex-initial p-2"
+            type="submit"
+            id="submit-btn"
+            aria-label="Search button"
+          >
+            <img className="col-" src={SearchIcon} alt="Magnifying Glass Search Icon" />
           </button>
         </form>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        lato: ["Lato", "sans-serif"],
+        lato: ["Roboto", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss/plugin')(function ({addBase}) {
+      addBase({
+        '[type="search"]::-webkit-search-decoration': {display: 'none'},
+        '[type="search"]::-webkit-search-cancel-button': {display: 'none'},
+        '[type="search"]::-webkit-search-results-button': {display: 'none'},
+        '[type="search"]::-webkit-search-results-decoration': {display: 'none'},
+      })
+    }),
+  ],
 };
