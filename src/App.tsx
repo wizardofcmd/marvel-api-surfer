1b4c965dcd08d9dad6a9385fab1e4078906b26dd
Sorin <sorinnechiforora@gmail.com>
2023-04-22 14:29:31 +0100

    Add title to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 function App() {
   return (
-    <>
-      <h1 className="font-lato">Hello world!</h1>
-    </>
+    <div className="flex flex-col items-center pt-1">
+      <h1 className="text-5xl font-bold">Marvel Comics Search Tool</h1>
+      <p className="text-neutral-500">Begin your journey by using the search input below.</p>
+    </div>
   );
 }
 
