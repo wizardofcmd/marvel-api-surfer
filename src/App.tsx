eaf5b1d3edcd9a13d6372234286aaae4dcb54ec7
Sorin <sorinnechiforora@gmail.com>
2023-04-23 16:52:59 +0100

    Create banner component for header and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,8 @@
-import { useState } from "react";
-import { fetchResults } from "./api/Request";
-import SearchForm from "./components/SearchForm";
+import Banner from "./components/Banner";
 
 function App() {
-  const [query, setQuery] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState("characters");
-
-  function handleUserInput(event: React.ChangeEvent<HTMLInputElement>) {
-    const target = event.target.value;
-    setQuery(target);
-  }
-
-  function handleSelectChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    const category = event.target.value;
-    setSelectedCategory(category);
-  }
-
-  function handleSearch(event: React.SyntheticEvent) {
-    event.preventDefault();
-    if (!query) return;
-
-    fetchResults(selectedCategory, query);
-  }
-
   return (
-    <div
-      className="flex h-screen w-screen flex-col gap-4 font-lato"
-      role="main"
-    >
-      <div className="flex flex-col items-center pt-1">
-        <h1 className="text-5xl font-bold">Marvel Comics Search Tool</h1>
-        <p className="text-neutral-500">
-          Begin your journey by using the search input below.
-        </p>
-      </div>
-
-      <div className="flex justify-center">
-        <SearchForm
-          category={selectedCategory}
-          value={query}
-          handleSearch={handleSearch}
-          handleUserInput={handleUserInput}
-          handleSelectChange={handleSelectChange}
-        />
-      </div>
-    </div>
+    <Banner/>
   );
 }
 
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -9,42 +9,44 @@ export default function SearchForm({
   handleSearch,
 }: AppProps) {
   return (
-    <form onSubmit={handleSearch} className="flex w-full justify-center">
-      <select
-        className="mr-1 h-10 w-40 flex-initial appearance-none
+    <div className="flex justify-center">
+      <form onSubmit={handleSearch} className="flex w-full justify-center">
+        <select
+          className="mr-1 h-10 w-40 flex-initial appearance-none
   border-2 border-slate-200 bg-white bg-[url('./assets/down-chevron.svg')] bg-right
   bg-no-repeat bg-origin-padding px-1 focus:outline-none"
-        value={category}
-        onChange={handleSelectChange}
-        name="category"
-        id="category"
-        aria-label="Dropdown menu for selecting categories to search by"
-      >
-        <option value="characters">Characters</option>
-        <option value="comics">Comics</option>
-        <option value="events">Events</option>
-        <option value="creators">Creators</option>
-        <option value="series">Series</option>
-        <option value="stories">Stories</option>
-      </select>
-      <input
-        className="appearance-none border-2 border-r-0 border-slate-200 px-1 focus:outline-none lg:h-10 lg:w-1/4"
-        type="search"
-        placeholder="Search"
-        id="search-input"
-        aria-label="Search input"
-        autoComplete="off"
-        value={value}
-        onChange={handleUserInput}
-      />
-      <button
-        className="w-10 flex-initial bg-sky-400 p-2 hover:bg-sky-500 lg:h-10"
-        type="submit"
-        id="submit-btn"
-        aria-label="Search button"
-      >
-        <img src={SearchIcon} alt="Magnifying Glass Search Icon" />
-      </button>
-    </form>
+          value={category}
+          onChange={handleSelectChange}
+          name="category"
+          id="category"
+          aria-label="Dropdown menu for selecting categories to search by"
+        >
+          <option value="characters">Characters</option>
+          <option value="comics">Comics</option>
+          <option value="events">Events</option>
+          <option value="creators">Creators</option>
+          <option value="series">Series</option>
+          <option value="stories">Stories</option>
+        </select>
+        <input
+          className="appearance-none border-2 border-r-0 border-slate-200 px-1 focus:outline-none h-10 w-1/4"
+          type="search"
+          placeholder="Search"
+          id="search-input"
+          aria-label="Search input"
+          autoComplete="off"
+          value={value}
+          onChange={handleUserInput}
+        />
+        <button
+          className="w-10 flex-initial bg-sky-400 p-2 hover:bg-sky-500 lg:h-10"
+          type="submit"
+          id="submit-btn"
+          aria-label="Search button"
+        >
+          <img src={SearchIcon} alt="Magnifying Glass Search Icon" />
+        </button>
+      </form>
+    </div>
   );
 }
