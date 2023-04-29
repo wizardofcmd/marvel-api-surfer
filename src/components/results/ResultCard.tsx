0288be21eeb6d6e07deb5e5692aa92ff4fdadf44
Sorin <sorinnechiforora@gmail.com>
2023-04-29 11:46:17 +0100

    Add styling for description and learn more button

--- src/components/results/ResultCard.tsx
+++ src/components/results/ResultCard.tsx
@@ -10,18 +10,20 @@ export function ResultCard({ ...result }: ResultProps) {
           alt={`Image of ${result.name}`}
         ></img>
       </div>
-      <div className="p-2">
+      <div className="p-2 flex flex-col gap-y-1">
         <div>
           <h2 className="text-xl font-semibold md:text-2xl">{result.name}</h2>
         </div>
         {result.description ? (
           <div>
-            <p>{result.description}</p>
+            <p className="italic">{result.description}</p>
           </div>
         ) : null}
         {result.detail ? (
-          <a href={result.detail}>
-            <button type="button">Learn more</button>
+          <a className="self-end" href={result.detail}>
+            <button className="bg-blue-700 p-1 px-2 my-2 rounded-md text-white" type="button">
+              Learn more
+            </button>
           </a>
         ) : null}
       </div>
